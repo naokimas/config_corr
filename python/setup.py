243ba8a6c25cbe033c3be6aa57b88077ad5389d4
sadamori_koujaku <sk16722@bristol.ac.uk>
2018-09-25 13:08:41 +0100

    modify the version code, add cvxpy as the required package, exclude python 2.7 from the list of compatible python versions

--- python/setup.py
+++ python/setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 from codecs import open
 
 setup(
-    name='config_corr',
-    version='1.0.1',
+    name='configcorr',
+    version='0.0.1',
     description='Python code for configuration model for correlation/covariance matrices',
     long_description='Python code for configuration model for correlation/covariance matrices',
     url='https://github.com/naokimas/config_corr',
@@ -15,12 +15,11 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Software Development',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     keywords='correlation matrix configuration model network',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=['scipy>=1.0', 'numpy', 'decorator'],
+    packages=find_packages(),
+    install_requires=['scipy>=1.0', 'numpy', 'cvxpy'],
 )
